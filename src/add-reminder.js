5d7acf160df8067afb3675454e997453780ed899
Jake Canlas <jakechristiancanlas1@gmail.com>
2026-01-22 19:59:15 -0500

    Added 'add' note button
    - added event listener for add note button

--- src/add-reminder.js
+++ src/add-reminder.js
@@ -1,1 +1,10 @@
-import { container } from "./main";
+import { addNoteBtn } from "./main";
+
+const addNote = function () {
+  addNoteBtn.addEventListener("click", (e) => {
+    const clicked = e.target;
+    // call function to create item here
+  });
+};
+
+export { addNote };
