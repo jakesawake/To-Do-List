b358cdc2b95a3eb4ba339d6e6a6c02d53080d177
Jake Canlas <jakechristiancanlas1@gmail.com>
2026-01-22 19:59:43 -0500

    Pseudo code for displayItems func

--- src/display-items.js
+++ src/display-items.js
@@ -1,3 +1,4 @@
-import { container } from "./main";
-
+// function to display a to-do item
+// - loop through the items values
+// - if there is a value then add a div
 const displayItems = function (item) {};
