2fd0e5f18d49745d632dbdbd85eae158cfd3e9c1
Jake Canlas <jakechristiancanlas1@gmail.com>
2026-01-22 20:00:13 -0500

    Selected DOM elements

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import "./style.css";
 import "./to-do-item.js";
 import "./display-items.js";
+import "./add-reminder.js";
+import { addNote } from "./add-reminder.js";
 
-const container = document.querySelector("#container");
+const container = document.querySelector(".container");
+const itemContainer = document.querySelector(".item-container");
+const sidebar = document.querySelector(".sidebar");
+const addNoteBtn = document.querySelector(".addNoteBtn");
 
-export { container };
+addNote();
+
+export { container, itemContainer, sidebar, addNoteBtn };
